7fb8f1cbe00f1ff7ba995f14a27d8e7c5b6e5c2f
Alexander Condello <acondello@dwavesys.com>
2017-07-14 12:38:26 -0700

    bug fixed

--- dwave_sapi_dimod.py
+++ dwave_sapi_dimod.py
@@ -1,6 +1,7 @@
 import sys
 import random
 import itertools
+import logging
 
 from dimod import DiscreteModelSampler, SpinResponse, ising_energy, qubo_to_ising
 from dimod.decorators import ising, ising_index_labels, qubo, qubo_index_labels
@@ -23,25 +24,37 @@ else:
 solver = local_connection.get_solver("c4-sw_optimize")
 A = get_chimera_adjacency(4, 4, 4)
 
+logging.basicConfig(format='%(levelname)s:%(funcName)s:%(message)s')
+log = logging.getLogger()
+
+
 class SAPISampler(DiscreteModelSampler):
 
     @ising(1, 2)
     @ising_index_labels(1, 2)
     def sample_ising(self, h, J, num_reads=1, **sapi_kwargs):
 
+        log.debug('INPUT h: %s', h)
+        log.debug('INPUT J: %s', J)
+        log.debug('INPUT num_reads: %s', num_reads)
+
+        # initialize the response object.
         response = SpinResponse()
 
         # if J is empty, then we are already done
         if not J:
+            log.debug('Empty J, determining sample from h')
             for __ in range(num_reads):
-                response.add_sample({v: _linear_to_spin(bias) for v, bias in iteritems(h)}, h=h, J=J)
+                response.add_sample({v: _linear_to_spin(bias)
+                                    for v, bias in iteritems(h)}, h=h, J=J)
             return response
 
         # add number of reads to SAPI
         sapi_kwargs['num_reads'] = num_reads
-        
+
         # convert h to a list
         h_list = [h[i] for i in range(len(h))]
+        log.debug('Converted h from %s to %s', h, h_list)
 
         # we need to know what variables we need
         S = {(v, v) for v in h}
@@ -50,6 +63,9 @@ class SAPISampler(DiscreteModelSampler):
         # find an embedding
         embeddings = find_embedding(S, A)
         [h0, j0, jc, embeddings] = embed_problem(h_list, J, embeddings, A)
+        log.debug('Embedded h0: %s', h0)
+        log.debug('Embedded j0: %s', j0)
+        log.debug('Embedded jc: %s', jc)
 
         # solve on sapi
         j_emb = j0
@@ -57,16 +73,16 @@ class SAPISampler(DiscreteModelSampler):
         answer = solve_ising(solver, h0, j_emb, **sapi_kwargs)
 
         # unembed
-        result = unembed_answer(answer['solutions'], embeddings, broken_chains='minimize_energy', h=h_list, j=J)
+        results = unembed_answer(answer['solutions'], embeddings,
+                                 broken_chains='minimize_energy', h=h_list, j=J)
 
         # parse the response
-        for soln, energy in zip(answer['solutions'], answer['energies']):
+        for soln, energy in zip(results, answer['energies']):
             sample = {v: soln[v] for v in h}
             response.add_sample(sample, energy)
 
         return response
 
-    response = SpinResponse()
     @ising(1, 2)
     @ising_index_labels(1, 2)
     def sample_structured_ising(self, h, J, num_reads=1, **sapi_kwargs):
@@ -75,8 +91,10 @@ class SAPISampler(DiscreteModelSampler):
 
         # if J is empty, then we are already done
         if not J:
+            log.debug('Empty J, determining sample from h')
             for __ in range(num_reads):
-                response.add_sample({v: _linear_to_spin(bias) for v, bias in iteritems(h)}, h=h, J=J)
+                response.add_sample({v: _linear_to_spin(bias) for v, bias in iteritems(h)},
+                                    h=h, J=J)
             return response
 
         # add number of reads to SAPI
@@ -84,6 +102,7 @@ class SAPISampler(DiscreteModelSampler):
 
         # convert h to a list
         h_list = [h[i] for i in range(len(h))]
+        log.debug('Converted h from %s to %s', h, h_list)
 
         # get the answer from sapi solver
         answer = solve_ising(solver, h_list, J, **sapi_kwargs)
--- tests/test_sapidimod.py
+++ tests/test_sapidimod.py
@@ -1,23 +1,35 @@
 import unittest
+import logging
 
 from dimod.samplers.tests.generic_sampler_tests import TestSolverAPI
-
 from dwave_sapi_dimod import SAPISampler
+from dwave_sapi_dimod import log as dimod_log
+
+# dimod_log.setLevel(logging.DEBUG)
+
 
 class TestSapiSampler(unittest.TestCase, TestSolverAPI):
+    """NOTE: This thing is an exact solver, so we can go ahead and
+    check the solution quality
+    """
     def setUp(self):
         self.sampler = SAPISampler()
-
 
     def test_basic(self):
         h = {0: -1, 1: 0, 2: 0, 3: 0, 4: -1}
         J = {(0, 4): -1}
 
         response = self.sampler.sample_structured_ising(h, {}, num_reads=10)
-        
+        sample = next(response.samples())
+        self.assertEqual(sample[0], 1)
+        self.assertEqual(sample[4], 1)
+
         response = self.sampler.sample_structured_ising(h, J, num_reads=10)
+        sample = next(response.samples())
+        self.assertEqual(sample[0], 1)
+        self.assertEqual(sample[4], 1)
 
         response = self.sampler.sample_ising(h, J, num_reads=10)
-
-        # print response
-
+        sample = next(response.samples())
+        self.assertEqual(sample[0], 1)
+        self.assertEqual(sample[4], 1)
